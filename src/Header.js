a21bc0c8c6fccec694fdcfddb39a8ed372eea7b5
Us Farag <usamax1@hotmail.com>
2020-11-13 13:28:55 +0100

    added styling

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="Trollface.png" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
