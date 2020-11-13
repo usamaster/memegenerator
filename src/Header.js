8264fc3a7950d07c5952c07c3b01fce3014451e5
Us Farag <usamax1@hotmail.com>
2020-11-13 13:54:54 +0100

    Changed header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import logo from "./Trollface.png"
 
 function Header() {
     return (
         <header>
             <img 
-                src="Trollface.png" 
+                src={logo}
                 alt="Problem?"
             />
             <p>Meme Generator</p>
